cb711dbd759dca90f525b7146695b799ec5d5262
Gabriela Lino <134440918+gabriela-lino1@users.noreply.github.com>
2024-11-08 16:06:15 -0300

    impar ou par

--- python/py.py
+++ python/py.py
@@ -1,3 +1,11 @@
-nome = (input('Nome: '))
+'''nome = (input('Nome: '))
 sobrenome = input('Sobrenome: ')
 print(nome + ' ' + sobrenome)
+'''
+
+numero = int(input('Me diga um número qualquer: '))
+if numero % 2 == 0:
+    print(f'O número {numero} é PAR')
+else:
+    print(f'O número {numero} é IMPAR')
+    
